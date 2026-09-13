c52d261a7c961d1de1a4f46dbb1523b837206506
RafaelYujiIkeda <rafaelikeda1620@gmail.com>
2026-09-13 00:17:18 -0300

    Primeiro commit

---
+++ meu-projeto/src/app/pagina-inicial/pagina-inicial.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pagina-inicial',
+  styleUrl: './pagina-inicial.css',
+  templateUrl: './pagina-inicial.html',
+})
+export class PaginaInicial {}
