d33d9725ab1d5c1bd5b6d0d538b3612118d9c911
RafaelYujiIkeda <rafaelikeda1620@gmail.com>
2026-09-13 21:38:21 -0300

    terceiro commit

--- meu-projeto/src/app/app.routes.ts
+++ meu-projeto/src/app/app.routes.ts
@@ -1,5 +1,18 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './cadastro/cadastro';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-
+{
+    path: '',
+    component: Cadastro
+},
+{
+    path: 'cadastro',
+    component: Cadastro
+},
+{
+    path: 'login',
+    component: Login
+}
 ];
